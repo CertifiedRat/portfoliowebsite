71791ccdf35892c5f0cd2b69268a6d628e216ec1
laura <laha1003@stud.ek.dk>
2026-06-06 08:55:54 +0200

    mobil menu lavet (glemte at stage all changes lol)

--- js/generel.js
+++ js/generel.js
@@ -6,3 +6,14 @@ function menuClicked() {
   menuIkon.classList.toggle("menuopen");
   nav.classList.toggle("menuopen");
 }
+
+//dropdown menuen, så den vises både ved hover(css) og click så den kan bruges i mobil.
+const ikon = document.querySelector(".dropdown-ikon");
+const indhold = document.querySelector(".dropdown-indhold");
+const dropdown = document.querySelector("#dropdown-menu");
+
+dropdown.addEventListener("click", dropdownClicked);
+function dropdownClicked() {
+  indhold.classList.toggle("dropdown-open");
+  ikon.classList.toggle("dropdown-open");
+}
