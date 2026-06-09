5c310a5f1362a3f8058a63f60ea70dba19fb33e4
laura <laha1003@stud.ek.dk>
2026-06-09 11:28:04 +0200

    indhold tilføjet til tema 2-5

--- js/generel.js
+++ js/generel.js
@@ -17,3 +17,40 @@ function dropdownClicked() {
   indhold.classList.toggle("dropdown-open");
   ikon.classList.toggle("dropdown-open");
 }
+
+//forstørrelse af billeder - notes to self
+//Udskifte indholdet af popovertarget vha. js, så jeg ikke skal indsætte samme billede 2 gange i html'en
+const popup = document.getElementById("imgpopup");
+const popoverImg = document.getElementById("imgpopover");
+
+//Det kan gøres ved at lave en funktion der sker (event) før popoveren vises,
+popup?.addEventListener("beforetoggle", function (event) {
+  console.log(event.newState);
+  //specificer at eventet kun skal ske hvis popoveren åbnes, ikke når den lukkes
+  //beforetoggle kan nemlig bruges både før-åbning og før-luk.
+  if (event.newState === "open") {
+    //Identificer den knap der er blevet trykket på(:focus) - dens popovertarget matcher vores const popup's id
+    const triggerBtn = document.querySelector(`[popovertarget="${popup.id}"]:focus`);
+    //tjek om den knap (popovertarget) har et billede nested i sig.
+    if (triggerBtn) {
+      const img = triggerBtn.querySelector("img");
+      //hvis den har, SÅ kan vi kopiere dets src og alt til det popoverens img tag
+      console.log(img.src, img.alt);
+      if (img) {
+        popoverImg.src = img.src;
+        popoverImg.alt = img.alt;
+      }
+    }
+  }
+});
+//Tl;DR - Sådan virker det:
+// 1. if: HVIS popoveren er VED at ÅBNE
+// 2. if: HVIS knappen der er trykket på, matcher vores popovers id
+// 3. if: HVIS der ligger et img-tag i knappen
+// 4. event: SÅ tag img-taggets src og alt, og giv deres værdi til det tomme img-tag der ligger i popoveren i HTML'en
+// 5. look at the bigger picture;))))
+
+//Nye ting jeg har lært;
+// beforetoggle = somewhere between click and popover??
+// bedre forståelse af at bruge if - betingelser/forudsætning for at kunne gå videre til næste skridt/aktion.
+//kilder: https://developer.mozilla.org/en-US/docs/Web/API/Popover_API/Using
